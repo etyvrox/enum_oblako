1f438a1cb24c8aa20b4320a58ebd934e9d55fa36
lisp3r <lisp3r@github.com>
2023-03-02 23:37:17 +0300

    Refactor "generate" pipeline: split gen() into smaller logical pieces, made all more pythonic. NOTE: in the middle of refactoring "enum" pipeline.

--- enum_oblako.py
+++ enum_oblako.py
@@ -1,73 +1,83 @@
 import click
 import os
-import sys
 import time
 import httpx
 import asyncio
-
+from itertools import product
 
 start_time = time.time()
-script_path = os.path.split(os.path.abspath(sys.argv[0]))[0]
+script_path = os.path.dirname(os.path.abspath(__file__))
 conf = {}
 url_list = []
+url_list_results = list()
 
 bucket_urls = [
-        'https://storage.yandexcloud.net/bucketname',           # yandex
-        'https://bucketname.hb.bizmrg.com',                     # vk_s3
-        'https://bucketname.ib.bizmrg.com',                     # vk_s3
-        'https://storage.cloud.croc.ru/bucketname',             # croc
-        'https://bucketname.selcdn.ru',                         # selectel_s3
+    'https://storage.yandexcloud.net/{bucketname}',             # yandex
+    'https://{bucketname}.hb.bizmrg.com',                       # vk_s3
+    'https://{bucketname}.ib.bizmrg.com',                       # vk_s3
+    'https://storage.cloud.croc.ru/{bucketname}',               # croc
+    'https://{bucketname}.selcdn.ru',                           # selectel_s3
 ]
 
 namespaces_urls = [
-        'https://namespace.s3mts.ru/bucketname',                # mts_s3
-        'https://namespace.s3pd01.sbercloud.ru/bucketname',     # sber_s3
-        'https://namespace.s3pd02.sbercloud.ru/bucketname',     # sber_s3
-        'https://namespace.s3pd11.sbercloud.ru/bucketname',     # sber_s3
-        'https://namespace.s3pd12.sbercloud.ru/bucketname',     # sber_s3
-        'https://namespace.s3pdgeob.sbercloud.ru/bucketname',   # sber_s3
+    'https://{namespace}.s3mts.ru/{bucketname}',                # mts_s3
+    'https://{namespace}.s3pd01.sbercloud.ru/{bucketname}',     # sber_s3
+    'https://{namespace}.s3pd02.sbercloud.ru/{bucketname}',     # sber_s3
+    'https://{namespace}.s3pd11.sbercloud.ru/{bucketname}',     # sber_s3
+    'https://{namespace}.s3pd12.sbercloud.ru/{bucketname}',     # sber_s3
+    'https://{namespace}.s3pdgeob.sbercloud.ru/{bucketname}',   # sber_s3
 ]
 
 saas_urls = [
-            'https://name.gitlab.yandexcloud.net',    # yandex gitlab
-            'https://name.website.yandexcloud.net',   # yandex website
-            'https://name.slack.com',                 # slack
-            'https://name.atlassian.net/login.jsp',   # atlassian
-            'https://name.my.salesforce.com',         # salesforce
-            # 'https://name.zendesk.com'              # zendesk
+    'https://{name}.gitlab.yandexcloud.net',                    # yandex gitlab
+    'https://{name}.website.yandexcloud.net',                   # yandex website
+    'https://{name}.slack.com',                                 # slack
+    'https://{name}.atlassian.net/login.jsp',                   # atlassian
+    'https://{name}.my.salesforce.com',                         # salesforce
+    # 'https://name.zendesk.com'                                # zendesk
 ]
+
+
+def read_payload_file(file_path):
+    """
+    Reads a file and returns a list of strings
+    """
+    with open(file_path) as file_obj:
+        return [line.strip() for line in file_obj.readlines()]
 
 
 @click.command()
 @click.option('--name', '-n', help='Enter company\'s name', default='')
 @click.option('--generate', help='Let cloudrecon generate all for you', type=bool, is_flag=True, default=False)
-@click.option('--namespaces', type=click.Path(exists=True), help='File with generated namespaces')
-@click.option('--buckets', type=click.Path(exists=True), help='File with generated bucket names')
+@click.option('--namespaces', type=click.Path(exists=True), help='File with generated namespaces',
+              default=os.path.join(script_path, 'namespaces.txt'))
+@click.option('--buckets', type=click.Path(exists=True), help='File with generated bucket names',
+              default=os.path.join(script_path, 'bucketnames.txt'))
 @click.option('--rps', '-rps', help='Enter number of requests per second to init', type=int, default=100)
-
-
 def cloudrec(name, generate, namespaces, buckets, rps):
-    global conf, start_time
+    global conf
 
     conf = {
-            'name': name,
-            'generate': generate,
-            'namespaces': namespaces,
-            'buckets': buckets,
-            'rps': rps
-            }
-    conf['timeout'] = 200 * round(100/conf['rps'], 2)
+        'name': name,
+        'namespaces': namespaces,
+        'buckets': buckets,
+        'rps': rps
+    }
+
+    conf['timeout'] = 200 * round(100 / conf['rps'], 2)
 
     if generate:
         gen(name)
-        print_stats()
-        asyncio.run(brute())
-        print(f" Time elapsed {str(time.time() - start_time)}")
     else:
         enum(name, namespaces, buckets)
-        print_stats()
-        asyncio.run(brute())
-        print("{*} Time elapsed - %s" % str(time.time() - start_time))
+
+    prepare_url_list()
+
+    print_stats()
+    asyncio.run(brute())
+
+    print(f"Time elapsed: {str(time.time() - start_time)}")
+    print(f'Fund {len(url_list_results)} results')
 
 
 def print_stats():
@@ -85,8 +95,8 @@ async def get(client, url):
     try:
         resp = await client.get(url)
         if resp.status_code not in failed_http_codes:
-            res = f"[+] Found - {url} : {str(resp.status_code)}"
-            print(res)
+            url_list_results.append({'url': url, 'code': resp.status_code})
+            print(f"[+] Found - {url} : {str(resp.status_code)}")
     except Exception:
         pass
 
@@ -96,68 +106,75 @@ def add(url):
     url_list.append(url)
 
 
+def prepare_url_list():
+    global url_list
+    url_list.sort()
+    url_list = list(set(url_list))
+
+
+def generate_mutations(company_name):
+    mutations = [company_name]
+
+    for mutation in read_payload_file(conf['namespaces']):
+        mutations.append(f"{mutation}-{company_name}")
+        mutations.append(f"{company_name}-{mutation}")
+    return mutations
+
+
 def gen(name):
-    mutations = []
-    conf['buckets'] = script_path + '/bucketnames.txt'
-    conf['namespaces'] = script_path + '/namespaces.txt'
+    mutations = generate_mutations(name)
 
-    bucketnames = [line.replace('\n', '') for line in open(script_path + '/bucketnames.txt')]
-    namespaces = [line.replace('\n', '') for line in open(script_path + '/namespaces.txt')]
-    
-    mutations.append(name)
-
-    for mutation in namespaces:
-        mutations.append(f"{mutation}-{name}")
-        mutations.append(f"{name}-{mutation}")
-    
-    enum_saas(mutations)    
-
-    for url in bucket_urls:
-        for mutation in mutations:
-            add(url.replace('bucketname', mutation))
-
-    for mutation in mutations:
-        for url in namespaces_urls:
-            for bucket in bucketnames:
-                add((url.replace('namespace', mutation)).replace('bucketname', bucket))
+    enum_saas(mutations)
+    enum_buckets(mutations)
+    enum_buckets_with_namespaces(mutations, conf['buckets'])
 
 
 def enum(name, namespaces, buckets):
-    temp = []
-    temp.append(name)
+    temp = [name]
     enum_saas(temp)
     enum_s3(temp, namespaces, buckets)
 
 
 def enum_s3(name, namespaces, buckets):
-    global start_time, bucket_urls, namespaces_urls
     temp_list = []
 
-    bucketnames = [bucket.replace('\n', '') for bucket in open(buckets)]
-    namespaces = [namespace.replace('\n', '') for namespace in open(namespaces)]
-    bucketnames += name
-
-    for namespace in namespaces:
+    for namespace in read_payload_file(namespaces):
         for url in namespaces_urls:
             temp_list.append(url.replace('namespace', namespace))
-    
-    bucket_urls += temp_list
+
+    bucketnames = read_payload_file(buckets) + name
 
     for bucket in bucketnames:
-        for url in bucket_urls:
+        for url in bucket_urls + temp_list:
             add(url.replace('bucketname', bucket))
 
 
-def enum_saas(name):
-    for mutation in name:
-        for url in saas_urls:
-            add(url.replace('name', mutation))
+def enum_saas(mutations):
+    for url in saas_urls:
+        for generated_saas_url in [url.format(name=mutation) for mutation in mutations]:
+            add(generated_saas_url)
+
+
+def enum_buckets(mutations):
+    for url in bucket_urls:
+        for generated_bucket_url in [url.format(bucketname=mutation) for mutation in mutations]:
+            add(generated_bucket_url)
+
+
+def enum_buckets_with_namespaces(mutations, buckets_file_path):
+    """
+    MTS Cloud S3: http(s)://NAMESPACE.s3mts.ru/BUCKET/file/
+    SberCloud S3: https://NAMESPACE.ZONE.sbercloud.ru/BUCKETNAME
+    """
+    with open(buckets_file_path) as buckets_f:
+        bucketnames = [line.strip() for line in buckets_f.readlines()]
+
+    for url in namespaces_urls:
+        for pair in [(ns, bucket) for ns, bucket in product(mutations, bucketnames)]:
+            add(url.format(namespace=pair[0], bucketname=pair[1]))
 
 
 async def brute():
-    global url_list, conf
-
-    url_list.sort()
     limits = httpx.Limits(max_connections=conf['rps'])
     tasks = []
 
